8142ca1107562912640b97de9590b8f6c33628fe
sunnybeatsza <makgerutumisho55@gmail.com>
2025-09-17 11:11:13 +0200

    added minimum numbers code

--- 2. Basic_Programming_Concepts/RandomNumbers/src/main/java/Main.java
+++ 2. Basic_Programming_Concepts/RandomNumbers/src/main/java/Main.java
@@ -1,30 +1,43 @@
 public class Main {
-//    Uniform random numbers.
-//    Write a program that prints five uniform random numbers between 0 and 1,
-//    their average value,
-//    and their minimum and maximum values.
-//    Use Math.random(), Math.min(), and Math.max().
+    //   Uniform random numbers.
+    //   Write a program that prints five uniform random numbers between 0 and 1,
+    //   their average value,
+    //   and their minimum and maximum values.
+    //   Use Math.random(), Math.min(), and Math.max().
 
-//    For these two exercises, use only Java features that
-//    we have introduced in this course so far (e.g,. no loops or conditionals).
+    //   For these two exercises, use only Java features that
+    //   we have introduced in this course so far (e.g. no loops or conditionals).
 
-// There is no input there is only an output.
-//1. The computer is supposed to
-// generate 5 random numbers and store them in variables.
-public static void main(String[] args) {
-    double randomNumber1 = Math.random();
-    double randomNumber2 = Math.random();
-    double randomNumber3 = Math.random();
-    double randomNumber4 = Math.random();
-    double randomNumber5 = Math.random();
+    // There is no input there is only an output.
+    // 1. The computer is supposed to
+    // generate 5 random numbers and store them in variables.
+    public static void main(String[] args) {
+        double randomNumber1 = Math.random();
+        double randomNumber2 = Math.random();
+        double randomNumber3 = Math.random();
+        double randomNumber4 = Math.random();
+        double randomNumber5 = Math.random();
 
-    System.out.println(randomNumber1);
-    System.out.println(randomNumber2);
-    System.out.println(randomNumber3);
-    System.out.println(randomNumber4);
-    System.out.println(randomNumber5);
+    //  The computer should calculate the average
+    //  of all 5 numbers and store it in a variable.
+    //  average = sum of all values / the number of values.
 
-//  The computer should calculate the average
-//  of all 5 numbers and store it in a variable.
-}
+        double sum = randomNumber1 + randomNumber2 + randomNumber3 + randomNumber4 + randomNumber5;
+        double averageNumber = sum / 5;
+
+
+        System.out.println(randomNumber1);
+        System.out.println(randomNumber2);
+        System.out.println(randomNumber3);
+        System.out.println(randomNumber4);
+        System.out.println(randomNumber5);
+        System.out.println("The average is: " + averageNumber);
+
+    //  The computer should determine the which number is the lowest
+        double lowestNumber = Math.min(randomNumber1, randomNumber2);
+        lowestNumber = Math.min(lowestNumber, randomNumber3);
+        lowestNumber = Math.min(lowestNumber, randomNumber4);
+        lowestNumber = Math.min(lowestNumber, randomNumber5);
+        System.out.println("The lowest number is: " + lowestNumber);
+    }
 }
