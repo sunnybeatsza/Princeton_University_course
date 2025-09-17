3b453e2d76f42c2e9ff6782ea7ca79f4c36e9a28
sunnybeatsza <makgerutumisho55@gmail.com>
2025-09-17 10:28:32 +0200

    updates to the first part of code

--- 2. Basic_Programming_Concepts/RandomNumbers/src/main/java/Main.java
+++ 2. Basic_Programming_Concepts/RandomNumbers/src/main/java/Main.java
@@ -1,2 +1,30 @@
 public class Main {
+//    Uniform random numbers.
+//    Write a program that prints five uniform random numbers between 0 and 1,
+//    their average value,
+//    and their minimum and maximum values.
+//    Use Math.random(), Math.min(), and Math.max().
+
+//    For these two exercises, use only Java features that
+//    we have introduced in this course so far (e.g,. no loops or conditionals).
+
+// There is no input there is only an output.
+//1. The computer is supposed to
+// generate 5 random numbers and store them in variables.
+public static void main(String[] args) {
+    double randomNumber1 = Math.random();
+    double randomNumber2 = Math.random();
+    double randomNumber3 = Math.random();
+    double randomNumber4 = Math.random();
+    double randomNumber5 = Math.random();
+
+    System.out.println(randomNumber1);
+    System.out.println(randomNumber2);
+    System.out.println(randomNumber3);
+    System.out.println(randomNumber4);
+    System.out.println(randomNumber5);
+
+//  The computer should calculate the average
+//  of all 5 numbers and store it in a variable.
+}
 }
