446d6311c73b6d82cddb1ce439fe41d940c625d5
sunnybeatsza <makgerutumisho55@gmail.com>
2025-09-21 20:09:52 +0200

    started off with 3 sort project

---
+++ 2. Basic_Programming_Concepts/ThreeSort/ThreeSortJava/src/main/java/ThreeSort.java
@@ -0,0 +1,24 @@
+public class ThreeSort {
+//
+//    Three-sort. Write a program
+//    that takes three integer command-line arguments and
+//    prints them in ascending order. Use Math.min() and Math.max().
+
+    public static void main(String[] args) {
+        System.out.println("this is the first number " + args[0]);
+        System.out.println("this is the second number " + args[1]);
+        System.out.println("this is the third number " + args[2]);
+    }
+    //Input = 3 numbers from command-line
+
+    //Process = Sorting them from smallest to biggest.
+
+    //Output = Display the numbers in ascending order.
+}
+
+
+// In order to test make sure to compile the file like below
+// javac ThreeSort.java
+
+// Then make sure to run the compiled file with 3 arguments like below
+// java ThreeSort 1 2 3
