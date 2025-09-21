c58adce298da3d0aa5e3d60594e47b15d3acbe26
sunnybeatsza <makgerutumisho55@gmail.com>
2025-09-21 20:31:44 +0200

    solved ThreeSort

--- 2. Basic_Programming_Concepts/ThreeSort/ThreeSortJava/src/main/java/ThreeSort.java
+++ 2. Basic_Programming_Concepts/ThreeSort/ThreeSortJava/src/main/java/ThreeSort.java
@@ -1,3 +1,5 @@
+import java.net.Inet4Address;
+
 public class ThreeSort {
 //
 //    Three-sort. Write a program
@@ -5,15 +7,38 @@ public class ThreeSort {
 //    prints them in ascending order. Use Math.min() and Math.max().
 
     public static void main(String[] args) {
+        //Input = 3 numbers from command-line
         System.out.println("this is the first number " + args[0]);
         System.out.println("this is the second number " + args[1]);
         System.out.println("this is the third number " + args[2]);
+
+        //Process = Sorting them from smallest to biggest.
+
+        // 1. Determine the lowest number first.
+        int lowestNumber = 0;
+        lowestNumber = Math.min(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        lowestNumber = Math.min(lowestNumber, Integer.parseInt(args[2]));
+
+        // 2. Determine the highest number next.
+        int highestNumber = 0;
+        highestNumber = Math.max(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        highestNumber = Math.max(highestNumber, Integer.parseInt(args[2]));
+
+        // 3. Then the number in the middle will be the link
+        int sum = Integer.parseInt(args[0]) + Integer.parseInt(args[1]) + Integer.parseInt(args[2]);
+        int middleNumber = sum - highestNumber;
+        middleNumber = middleNumber - lowestNumber;
+
+
+        //Output = Display the numbers in ascending order.
+        System.out.println(lowestNumber);
+        System.out.println(middleNumber);
+        System.out.println(highestNumber);
+
     }
-    //Input = 3 numbers from command-line
 
-    //Process = Sorting them from smallest to biggest.
 
-    //Output = Display the numbers in ascending order.
+
 }
 
 
@@ -21,4 +46,4 @@ public class ThreeSort {
 // javac ThreeSort.java
 
 // Then make sure to run the compiled file with 3 arguments like below
-// java ThreeSort 1 2 3
+// java ThreeSort 10 2 21
